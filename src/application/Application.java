0d5a7f72d16ccd43f36eab578cf964ab5398b202
Ruth METAYER <METAYER_Ru@lprs.local>
2025-03-04 10:20:40 +0100

    feat():Commencement du main

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,17 +1,20 @@
 package application;
 
-/*import zoo.animal.*;
-import zoo.Personne.*;
-import zoo.Enclos;*/
 import zoo.*;
+import zoo.Personne.Personne;
+import zoo.Personne.Soigneur;
+import zoo.Personne.Visiteur;
+
 public class Application {
     public static void main(String[] args) {
+        Zoo zoo=new Zoo("Jardin des Plantes","Paris");
+        Personne soigneur1 = new Soigneur("Michelinne","Eve","Serpent");
+        Personne soigneur2= new Soigneur("Goudenough","David","Lion");
+        Personne soigneur3= new Soigneur("Popo","Paulette","Ouiseaux");
+        Personne visiteur1 = new Visiteur("Castano","Maite",25);
+        Personne visiteur2= new Visiteur("Macario","Vincent",40);
+        Personne visiteur3= new Visiteur("Cho","Juliette",4);
 
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-
-             System.out.println("i = " + i);
-        }
     }
 }
